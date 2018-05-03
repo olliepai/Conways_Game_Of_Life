85a57f4dc1fe5a1b1f5b010c24e03a2d5603f76c
DaveDLeague <dave.dunn@jointheleague.org>
2018-05-03 09:22:19 -0700

    Add files via upload

--- src/Cell.java
+++ src/Cell.java
@@ -1,29 +1,64 @@
+import java.awt.Color;
 import java.awt.Graphics;
-import java.util.ArrayList;
 
 public class Cell implements Drawable{
+	public boolean isAlive = false;
+	
 	private int x;
 	private int y;
-	private boolean isAlive;
+
+	private int cellSize;
 	
-	public void liveOrDie(ArrayList<Cell> neighbors) {
-		//sets isAlive to true or false based on the neighbors and 
-		//the rules of the game
-		/*
-		 * 1. Any live cell with fewer than two live nieghbours dies, as if caused by underpopulation.
-		 * 2. Any live cell with two or three live neighbours lives on to the next generation.
-		 * 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
-		 * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-		 * (source: Wikipedia)
-		 * */
-		
+	
+	public Cell(int x, int y, int size) {
+		this.x = x;
+		this.y = y;
+		this.cellSize = size;
 	}
 	
-	@Override
-	public void draw(Graphics g) {
-		//draws colored square if cell is alive
-		//draws empty square if cell is dead
-		
+	//sets isAlive to true or false based on the neighbors and 
+	//the rules of the game
+	/*
+	 * 1. Any live cell with fewer than two live nieghbours dies, as if caused by underpopulation.
+	 * 2. Any live cell with two or three live neighbours lives on to the next generation.
+	 * 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+	 * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+	 * (source: Wikipedia)
+	 * */
+	public void liveOrDie(int numNeighbors) {
+		if(isAlive) {
+			if(numNeighbors < 2 || numNeighbors > 3) {
+				isAlive = false;
+			}
+		}else {
+			if(numNeighbors == 3) {
+				isAlive = true;
+			}
+		}
+	}
+	
+	public int getX() {
+		return x;
 	}
 
+	public int getY() {
+		return y;
+	}
+	
+	//draws colored square if cell is alive
+	//draws empty square if cell is dead
+	@Override
+	public void draw(Graphics g) {
+	
+		if(isAlive) {
+			g.setColor(Color.BLUE);
+			g.fillRect(x, y, cellSize, cellSize);
+		} else {
+			g.setColor(Color.LIGHT_GRAY);
+			g.fillRect(x, y, cellSize, cellSize);
+		}
+		//border
+		//g.setColor(Color.BLACK);
+		//g.drawRect(x, y, cellSize, cellSize);
+	}
 }
--- src/WorldPanel.java
+++ src/WorldPanel.java
@@ -1,25 +1,170 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Random;
 
-public class WorldPanel implements Drawable {
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class WorldPanel extends JPanel implements MouseListener, ActionListener {
+	private int cellsPerRow;
+	private int cellSize;
 	private Cell[][] cells;
+	private Timer timer;
+	
+	public WorldPanel(int w, int h, int cpr) {
+		setPreferredSize(new Dimension(w, h));
+		addMouseListener(this);
+		timer = new Timer(500, this);
+		this.cellsPerRow = cpr;
+	
+		cellSize = h / cellsPerRow;
+		System.out.println(cellSize);
+		cells = new Cell[cellsPerRow][cellsPerRow];
+		for(int i = 0; i < cells.length; i++) {
+			for(int j = 0; j < cells[0].length; j++) {
+				cells[i][j] = new Cell(j * cellSize, i * cellSize, cellSize);
+			}
+		}
+	}
+	
+	public void randomizeCells() {
+		for(int i = 0; i < cells.length; i++) {
+			for(int j = 0; j < cells[0].length; j++) {
+				cells[i][j].isAlive = new Random().nextBoolean();
+			}
+		}
+		repaint();
+	}
+	
+	public void clearCells() {
+		for(int i = 0; i < cells.length; i++) {
+			for(int j = 0; j < cells[0].length; j++) {
+				cells[i][j].isAlive = false;
+			}
+		}
+		repaint();
+	}
+	
+	public void startAnimation() {
+		timer.start();
+	}
+	
+	public void stopAnimation() {
+		timer.stop();
+	}
+	
+	public void setAnimationDelay(int sp) {
+		timer.setDelay(sp);
+	}
 	
 	@Override
-	public void draw(Graphics g) {
-		//iterate through cells and draw them
+	public void paintComponent(Graphics g) {
+		for(int i = 0; i < cells.length; i++) {
+			for(int j = 0; j < cells[0].length; j++) {
+				cells[i][j].draw(g);
+			}
+		}
 		
+		g.setColor(Color.BLACK);
+		g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
 	}
 	
+	//advances world one step
 	public void step() {
-		//advances world one step
 		//iterate through cells and get their neighbors
 		//check if each cell should live or die
+		int[][] numLivingNbors = new int[cellsPerRow][cellsPerRow];
+		for(int i = 0; i < cellsPerRow; i++) {
+			for(int j = 0; j < cellsPerRow; j++) {
+				numLivingNbors[i][j] = getLivingNeighbors(i, j);
+			}
+		}
+		for(int i = 0; i < cellsPerRow; i++) {
+			for(int j = 0; j < cellsPerRow; j++) {
+				cells[i][j].liveOrDie(numLivingNbors[i][j]);
+			}
+		}
+		repaint();
 	}
 	
-	public ArrayList<Cell> getNeighbors(int x, int y){
-		//returns an array list of the  8 or less neighbors of the 
-		//cell identified by x and y
-		return null;
+	//returns an array list of the  8 or less neighbors of the 
+	//cell identified by x and y
+	public int getLivingNeighbors(int x, int y){
+		int livingNeighbors = 0;
+		
+		if(x != 0) {
+			if(cells[x - 1][y].isAlive) livingNeighbors++;
+		}
+		
+		if(x != cellsPerRow - 1) {
+			if(cells[x + 1][y].isAlive) livingNeighbors++;
+		}
+		if(y != 0) {
+			if(cells[x][y - 1].isAlive) livingNeighbors++;
+		}
+		if(y != cellsPerRow - 1) {
+			if(cells[x][y + 1].isAlive) livingNeighbors++;
+		}
+		if(x != 0 && y != 0) {
+			if(cells[x - 1][y - 1].isAlive) livingNeighbors++;
+		}
+		if(x != cellsPerRow - 1 && y != cellsPerRow - 1) {
+			if(cells[x + 1][y + 1].isAlive) livingNeighbors++;
+		}
+		if(x != 0 && y != cellsPerRow - 1) {
+			if(cells[x - 1][y + 1].isAlive) livingNeighbors++;
+		}
+		if(x != cellsPerRow - 1 && y != 0) {
+			if(cells[x + 1][y - 1].isAlive) livingNeighbors++;
+		}
+		
+		return livingNeighbors;
 	}
 
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		for(int i = 0; i < cellsPerRow; i++) {
+			for(int j = 0; j < cellsPerRow; j++) {
+				Cell c = cells[i][j];
+				if(e.getX() > c.getX() && e.getX() < (c.getX() + cellSize) &&
+				   e.getY() > c.getY() && e.getY() < (c.getY() + cellSize)) {
+					c.isAlive = !c.isAlive;
+				}
+			}
+		}
+		repaint();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		step();		
+	}
 }
